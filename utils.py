56cc02dd449ab79c9accd6707a3e7bdb8e267ed4
Nicolás Venegas Oliva <nvenegas.oliva@gmail.com>
2018-11-15 15:34:07 -0300

    docs: Add docstring to load_function method

--- utils.py
+++ utils.py
@@ -4,7 +4,22 @@ from os.path import isfile, join
 import json
 
 
-def load_datasets():
+def load_datasets(path='datasets/'):
+    """Load all dataset in memory.
+
+    This function load all tweets datasets from 'datasets' folder to a
+    dictionary where the key is name of file.
+
+    Args:
+        arg1 (string): Path used to find the datasets.
+    Returns:
+        dict: different datasets in path like dictionary.
+
+    Examples:
+        >>> datasets = load_datasets()
+        >>> datasets['theinterngroup']
+        [tweet_1, tweet_2, tweet_3, ... , tweet_n]
+    """
     path = 'datasets/'
     files = [f for f in listdir(path) if isfile(join(path, f))]
 
